025836442bcf58233a3753d476c785a62bc248c2
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2013-08-30 05:00:34 +0300

    added show tags

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -7,6 +7,9 @@
 #= require markitup/sets/markdown/set
 #= require select2
 
+
+
+
 jQuery ->
   $("textarea").markItUp markdownSettings
   $("#book_tag_list").select2({tags: $("#book_tag_list").data('autocomplete-source'), width: 240}  )
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
     options = {
@@ -12,8 +13,30 @@ module ApplicationHelper
     }
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
+
   def category_filter(id)
     #redirects_to Book.where(category_id: id)
     redirect_to @book
   end
+
+  def get_tag_size_render
+    40
+  end
+
+  def get_tag_count
+    Book.tag_counts.size
+  end
+
+  def get_tag_size tag
+    tag.count
+  end
+
+  def get_tag_array
+    tag_array = ''
+    tag_cloud Book.tag_counts, %w{s m l} do |tag, css_class|
+      tag_array << '{text:"'+tag.name+'", weight:'+get_tag_size(tag).to_s+', link:"/tags/'+tag.name+'"},'
+    end
+    raw('[' + tag_array + ']')
+  end
+
 end
