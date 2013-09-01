ac8bd01ec148c606392e38fd05c7880270be603e
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2013-09-01 17:25:23 +0300

    add mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_valid_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def switch_theme
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
       cookies[:style] = 'black'
     end
     redirect_to :back
+  end
+
+  def configure_valid_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  ## :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  has_many :books, dependent: :destroy
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
