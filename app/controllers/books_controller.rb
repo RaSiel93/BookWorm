883a8ab0c93932b14f1a31e824733e26df62a832
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2013-09-01 04:36:21 +0300

    added buttons in show_chapters and light in index_book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -67,7 +67,7 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to book_path }
+      format.html { redirect_to books_url }
       format.json { head :no_content }
     end
   end
