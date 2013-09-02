00e9c3f7c1aa8354b13df10560bc7b3efecbaa61
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2013-09-02 23:34:51 +0300

    locale complite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :switch_locale_before
   before_filter :configure_valid_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
@@ -16,10 +17,16 @@ class ApplicationController < ActionController::Base
   def switch_locale
     if cookies[:locale] == 'ru'
       cookies[:locale] = 'en'
+      I18n.locale = 'en'
     else
       cookies[:locale] = 'ru'
+      I18n.locale = 'ru'
     end
     redirect_to :back
+  end
+
+  def switch_locale_before
+    I18n.locale = cookies[:locale] == 'ru'?'ru':'en'
   end
 
   def configure_valid_parameters
